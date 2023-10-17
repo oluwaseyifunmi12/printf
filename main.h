e8226f0a6dd6301f3cdb13844068826b6a3b3c00
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-10-17 04:06:19 -0700

    correcting printf

--- main.h
+++ main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+
 int _printf(const char *format, ...);
+
 
 #endif /* MAIN_H */
--- printf.c
+++ printf.c
@@ -1,55 +1,56 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h> /* Include the <stdio.h> header for sprintf */
 
+/**
+ * _printf - a function that works similar to the standard printf
+ * @format: first argument
+ * Return: the number of characters printed to out_put
+ */
 int _printf(const char *format, ...) {
-    va_list args;
-    int char_count = 0;
+	va_list args;
+	int char_count = 0;
 
-    va_start(args, format);
+	va_start(args, format);
+	
+	while (format && *format) {
+	if	(*format != '%') {
+		char_count += write(1, format, 1);
+	} else {
+		format++;
+		switch (*format) {
+		case 'c': {
+			char c = va_arg(args, int);
+			char_count += write(1, &c, 1);
+			break;
+		}
+		case 's': {
+			char *s = va_arg(args, char *);
+			while (*s) {
+			char_count += write(1, s, 1);
+			s++;
+		}
+		break;
+		}
+		case 'd':
+		case 'i': {
+			int num = va_arg(args, int);
+			char num_str[20];
+			int len = sprintf(num_str, "%d", num);
+			char_count += write(1, num_str, len);
+			break;
+		}
+		case '%': {
+			char_count += write(1, "%", 1);
+			break;
+                }
+		default: {
+			char_count += write(1, "%", 1);
+			char_count += write(1, format, 1);
+		}
+		}
+	}
+	format++;
+	}
 
-    while (format && *format) {
-        if (*format != '%') {
-            char_count += write(1, format, 1); /* Write a character to stdout */
-        } else {
-            format++; /* Move past '%' */
-            switch (*format) {
-                case 'c': {
-                    char c = va_arg(args, int); /* char is promoted to int in varargs */
-                    char_count += write(1, &c, 1);
-                    break;
-                }
-                case 's': {
-                    char *s = va_arg(args, char *);
-                    while (*s) {
-                        char_count += write(1, s, 1);
-                        s++;
-                    }
-                    break;
-                }
-                case 'd':
-                case 'i': {
-                    int num = va_arg(args, int);
-                    char num_str[20]; /* A reasonable buffer size for an int */
-                    int len = sprintf(num_str, "%d", num);
-                    char_count += write(1, num_str, len);
-                    break;
-                }
-                case '%': {
-                    char_count += write(1, "%", 1);
-                    break;
-                }
-                default: {
-                    char_count += write(1, "%", 1); /* Handle unknown conversion specifiers */
-                    char_count += write(1, format, 1);
-                }
-            }
-        }
-        format++;
-    }
-
-    va_end(args);
-    return char_count;
+	va_end(args);
+	return char_count;
 }
-
